08f341739f65385b86bcbff27364fc7259d504fd
BishopSwearingen <111836157+BishopSwearingen@users.noreply.github.com>
2023-10-02 18:57:43 -0500

    Create main.go

---
+++ Assignments/P01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/BishopSwearingen/4143-PLC/mascot"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(mascot.BestMascot())
+	fmt.Println(quote.Go())
+
+}
